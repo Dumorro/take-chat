c28bf60d0576afab8a315dbffbf3e0ea4702c5fd
Dumorro <dumorro@gmail.com>
2020-06-01 11:19:21 -0300

    Bate papo - adicionar e remover participante.

---
+++ src/Take.BatePapo.Servidor/Take.BatePapo.Dominio/Entidades/SalaDeBatePapo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+
+namespace Take.BatePapo.Dominio.Entidades
+{
+    public class SalaDeBatePapo
+    {
+        private readonly ConcurrentDictionary<string, WebSocket> _participantes = new ConcurrentDictionary<string, WebSocket>();
+
+        public IEnumerable<string> Participantes => _participantes.Select(k => k.Key);
+
+        public bool AdicionarParticipante(string apelido, WebSocket socket) => _participantes.TryAdd(apelido, socket);
+
+        public bool RemoverParticipante(string apelido) => _participantes.TryRemove(apelido, out var socket);
+    }
+}
---
+++ src/Take.BatePapo.Servidor/Take.BatePapo.TestesDeUnidade/Dominio/SalaDeBatePapoTeste.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using ExpectedObjects;
+using Take.BatePapo.Dominio.Entidades;
+using Xunit;
+
+namespace Take.BatePapo.TestesDeUnidade.Dominio
+{
+    public class SalaDeBatePapoTeste
+    {
+        [Fact]
+        public void DeveAdicionarUsuarioAoBatePapo()
+        {
+            var participanteApelido = "José";
+            var particitpantesDoBatePapoEsperados = new List<string>{participanteApelido};
+            var batePapo = new SalaDeBatePapo();
+            batePapo.AdicionarParticipante(participanteApelido, new ClientWebSocket());
+
+            var particitpantesDoBatePapo = batePapo.Participantes;
+
+            particitpantesDoBatePapo.ToExpectedObject().ShouldMatch(particitpantesDoBatePapoEsperados);
+        }
+
+        [Fact]
+        public void NaoDeveAdicionarUsuarioJaExistenteAoBatePapo()
+        {
+            var participanteApelido = "José";
+            var particitpantesDoBatePapoEsperados = new List<string> { participanteApelido };
+            var batePapo = new SalaDeBatePapo();
+            batePapo.AdicionarParticipante(participanteApelido, new ClientWebSocket());
+            //Adicionando usuário repetido
+            var usuarioFoiAdiconado = batePapo.AdicionarParticipante(participanteApelido, new ClientWebSocket());
+
+            var particitpantesDoBatePapo = batePapo.Participantes;
+
+            Assert.False(usuarioFoiAdiconado);
+            particitpantesDoBatePapo.ToExpectedObject().ShouldMatch(particitpantesDoBatePapoEsperados);
+        }
+        
+        [Fact]
+        public void DeveRemoverUsuarioExistenteNoBatePapo()
+        {
+            //criar bate papo e adicionar participante à sala.
+            var apelidoDoParticipante = "José";
+            var particitpantesDoBatePapoEsperados = new List<string> { apelidoDoParticipante };
+            var batePapo = new SalaDeBatePapo();
+            batePapo.AdicionarParticipante(apelidoDoParticipante, new ClientWebSocket());
+            var particitpantesDoBatePapo1Participante = batePapo.Participantes;
+            var totalInicialDeParticipantes = particitpantesDoBatePapo1Participante.Count();
+            
+            Assert.True(totalInicialDeParticipantes == 1);
+
+            var participanteRemovidoDoBatePapo = batePapo.RemoverParticipante(apelidoDoParticipante);
+            var totalDeParticipantesObtido = batePapo.Participantes.Count();
+
+            Assert.True(participanteRemovidoDoBatePapo);
+            Assert.True(totalDeParticipantesObtido==0);
+            Assert.Empty(batePapo.Participantes);
+        }
+    }
+}
