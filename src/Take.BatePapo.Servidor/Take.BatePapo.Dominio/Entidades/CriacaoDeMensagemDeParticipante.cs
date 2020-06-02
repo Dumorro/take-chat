64565b5cfa4ef9047d43b151b6f0ee7dc6d807c0
Thiago F Coelho <thiago.coelho@bs2.com>
2020-06-02 11:55:54 -0300

    Update CriacaoDeMensagemDeParticipante.cs

--- src/Take.BatePapo.Servidor/Take.BatePapo.Dominio/Entidades/CriacaoDeMensagemDeParticipante.cs
+++ src/Take.BatePapo.Servidor/Take.BatePapo.Dominio/Entidades/CriacaoDeMensagemDeParticipante.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Take.BatePapo.Dominio.Dtos;
+﻿using Take.BatePapo.Dominio.Dtos;
 
 namespace Take.BatePapo.Dominio.Entidades
 {
     public class CriacaoDeMensagemDeParticipante
     {
-        private const string _modeloMensgem = "[{0}] fala para {1}: {2}";
+        private const string _modeloMensgem = "[{0}] fala para [{1}]: {2}";
         public static string Montar(Mensagem mensagem)
         {
             return string.Format(_modeloMensgem, mensagem.ApelidoDoParticipante, mensagem.Destinatario, mensagem.Texto);
